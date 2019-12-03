610d5ebedd58193a02a7455f4d4b2babf590bbb6
Jean-Paul Lizotte <jean-paul.lizotte@emyode.com>
2019-12-03 10:40:54 -0500

    -Added a script to download the repo

---
+++ scripts/get_git.ps1
@@ -0,0 +1,25 @@
+﻿param([string]$repo="https://github.com/emyode/InfraTemplate.git")
+Install-Module posh-git -Scope CurrentUser -Force
+
+Install-Module posh-git -Scope CurrentUser #-AllowPrerelease -Force # Newer beta version with PowerShell Core support
+
+Import-Module posh-git
+cd $PSScriptRoot
+$newpath = (join-path -Path PSScriptRoot -ChildPath "Repo")
+mkdir $newpath -Force -ErrorAction Ignore
+cd $newpath
+try {
+$result = invoke-expression "git clone $repo -q --progress"
+}
+catch
+{
+    Write-Output $_.Exception.Message
+
+}
+$result
+
+
+
+
+
+
